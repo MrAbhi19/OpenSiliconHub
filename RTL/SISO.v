072570a99aefc2f2e0ef7a50d18d3b7ff512d688
ABHILASH MADDINENI <214151262+MrAbhi19@users.noreply.github.com>
2025-11-16 17:44:52 +0530

    Update SISO.v

--- RTL/SISO.v
+++ RTL/SISO.v
@@ -1,11 +1,11 @@
 module SISO #(
-  parameter SIZE=8
+  parameter WIDTH=8
 )(
   input wire in,clk,enable,reset,
   output reg out,done,busy
 );
-  reg [$clog2(SIZE)-1:0] bit_count;
-  reg [SIZE-1:0] shift_reg;
+  reg [$clog2(WIDTH)-1:0] bit_count;
+  reg [WIDTH-1:0] shift_reg;
   
   always@(posedge clk or posedge reset) begin
     if (reset) begin
@@ -15,11 +15,11 @@ module SISO #(
       out<=0;
     end
     else if (enable) begin
-      shift_reg<={shift_reg[SIZE-2:0],in};
-      out<=shift_reg[SIZE-1];
+      shift_reg<={shift_reg[WIDTH-2:0],in};
+      out<=shift_reg[WIDTH-1];
       busy<=1;
 
-      if (bit_count==$clog2(SIZE)'(SIZE-1)) begin
+      if (bit_count==$clog2(WIDTH)'(WIDTH-1)) begin
         bit_count<=0;
         done<=1;
         busy<=0;
