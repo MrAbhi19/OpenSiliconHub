afe3dfba9a383108087a5c880260be604b7788e9
ABHILASH MADDINENI <214151262+MrAbhi19@users.noreply.github.com>
2025-11-16 14:44:01 +0530

    Update MAC.v

--- SRC/MAC.v
+++ SRC/MAC.v
@@ -1,9 +1,11 @@
 module MAC #(
-  parameter WIDTH=8
+  parameter WIDTH_A=8,
+  parameter WIDTH_B=12
 )(
-  input [WIDTH-1:0] A,B,
-  input [2*WIDTH-1:0] C,
-  output [2*WIDTH-1:0] Y
+  input [WIDTH_A-1:0] A,
+  input [WIDTH_B-1:0] B,
+  input [WIDTH_A+WIDTH_B-1:0] C,
+  output [WIDTH_A+WIDTH_B-1:0] Y
 );
   assign Y=(A*B)+C;
 endmodule
