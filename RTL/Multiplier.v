9519d3c240212fa3b2074857895f88fc6617c111
ABHILASH MADDINENI <214151262+MrAbhi19@users.noreply.github.com>
2025-11-25 15:59:59 +0530

    Update Multiplier.v
    Added pipelining so that the module is reliable at high frequencies

--- RTL/Multiplier.v
+++ RTL/Multiplier.v
@@ -1,10 +1,29 @@
-module Multiplier #(
-  parameter WIDTH_A=4,
-  parameter WIDTH_B=6
+module Multiplier_piped #(
+  parameter WIDTH_A = 4,
+  parameter WIDTH_B = 6,
+  parameter DELAY   = 5   // number of pipeline stages
 )(
-  input [WIDTH_A-1:0] in1,
-  input [WIDTH_B-1:0] in2,
+  input                        clk,
+  input  [WIDTH_A-1:0]        in1,
+  input  [WIDTH_B-1:0]        in2,
   output [WIDTH_A+WIDTH_B-1:0] out
 );
-  assign out=in1*in2;
+
+  // Raw product
+  wire [WIDTH_A+WIDTH_B-1:0] mult_raw = in1 * in2;
+
+  // Pipeline registers
+  reg [WIDTH_A+WIDTH_B-1:0] pipe_reg [0:DELAY-1];
+
+  integer i;
+
+  always @(posedge clk) begin
+    pipe_reg[0] <= mult_raw;
+    for (i = 1; i < DELAY; i = i + 1) begin
+      pipe_reg[i] <= pipe_reg[i-1];
+    end
+  end
+
+  assign out = pipe_reg[DELAY-1];
+
 endmodule
